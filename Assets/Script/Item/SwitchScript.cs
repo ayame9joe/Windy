b826ab6ab68e56e2e686a4b15661718f6c32dc5d
Qiao Xiaomeng <ayame9joe@gmail.com>
2016-01-25 15:01:32 +0800

    switch test

--- Assets/Script/Item/SwitchScript.cs
+++ Assets/Script/Item/SwitchScript.cs
@@ -6,6 +6,8 @@ public class SwitchScript : MonoBehaviour {
 
 	public string switchName;
 
+	bool switchOnce;
+
 	void OnEnable ()
 	{
 		EventManager.StartListening ("SwitchOn", SwitchOn);
@@ -20,9 +22,30 @@ public class SwitchScript : MonoBehaviour {
 	{
 		
 		if (switchName == "level1Up") {
-			//Debug.Log ("Switch On");
-			GameObject.Find ("Level1WindyPassage1").transform.DOMove (new Vector3 (9, 7.5f, -10), 2);
-
+			if (!switchOnce) {
+				StartCoroutine ("Level1Up");
+				switchOnce = true;
+			}
 		}
+	}
+
+	IEnumerator Level1Up ()
+	{
+		//Debug.Log ("Level 1 Up");
+		yield return new WaitForSeconds (1);
+		if (GameObject.Find ("Level1WindPassage1")) {
+			GameObject.Find ("Level1WindPassage1").SetActive (false);
+		}
+		//Debug.Log (4);
+		GameObject.Find ("Level1WindyPassage1Slice4").transform.DOMove (new Vector3 (9, 7.5f, -8), 2);
+		yield return new WaitForSeconds (1);
+		//Debug.Log (3);
+		GameObject.Find ("Level1WindyPassage1Slice3").transform.DOMove (new Vector3 (9, 7.5f, -6), 2);
+		yield return new WaitForSeconds (1);
+		//Debug.Log (2);
+		GameObject.Find ("Level1WindyPassage1Slice2").transform.DOMove (new Vector3 (9, 7.5f, -4), 2);
+		yield return new WaitForSeconds (1);
+		//Debug.Log (1);
+		GameObject.Find ("Level1WindyPassage1Slice1").transform.DOMove (new Vector3 (9, 7.5f, -2), 2);
 	}
 }
